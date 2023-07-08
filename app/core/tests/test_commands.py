9fc6e17c4c8d121558d084542f5f250fed6a4e5a
Muhammed Basheer <basheerkomassery@gmail.com>
2023-07-08 16:35:42 +0530

    Edited test_commands.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
     """Test Commands."""
