1b6c20cfbd25c96fa1daefde007ebe3297daff57
Muhammed Basheer <basheerkomassery@gmail.com>
2023-07-10 13:28:35 +0530

    Created model for users in the project and manager to create, save and return saved user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,38 @@
-from django.db import models # noqa
+"""Create user models"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Model for all users in the project. including,
+    customer, seller/vendor, admin.
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=14, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_seller = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
