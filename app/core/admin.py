6a05499ce5b334c30beef7023d220318f2c33cc6
Muhammed Basheer <basheerkomassery@gmail.com>
2023-07-18 18:03:52 +0530

    Registered user model in admin and wrote unit tests for admin page user listing.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin # noqa
+"""Django admin customization."""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users."""
+    ordering = ['id']
+    list_display = ['email','mobile','name']
+admin.site.register(models.User, UserAdmin)
