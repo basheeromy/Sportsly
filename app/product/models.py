c36510dac4e6c3f96964a8560c013936275cabb3
Muhammed Basheer <basheerkomassery@gmail.com>
2023-08-03 12:57:52 +0530

    Edited category model.

--- app/product/models.py
+++ app/product/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
     """Manage product catergories with hierarchy."""
 
     name = models.CharField(max_length=100, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.name
