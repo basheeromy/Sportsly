597b6592736e9bf51e14060a8f093a47e81d6734
Muhammed Basheer <basheerkomassery@gmail.com>
2023-08-10 18:50:22 +0530

    edited imports and canged Product_items_list view to product list view

--- app/product/views.py
+++ app/product/views.py
@@ -1,14 +1,15 @@
-from rest_framework.views import APIView
+
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from .models import Product_item
+from .models import Product
 from .serializers import ProductSerializer
 from drf_spectacular.utils import extend_schema
 
-class ProductListView(APIView):
+class ProductListView(ListAPIView):
 
     @extend_schema(request=ProductSerializer, responses=None)
     def get(self, request, format=None):
-        products = Product_item.objects.all()
+        products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
 
         return Response(serializer.data)
