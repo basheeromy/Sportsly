dc09de31a8ef7b40a6f15bbd4be21e897d899178
Muhammed Basheer <basheerkomassery@gmail.com>
2023-09-13 17:50:07 +0530

    Edited urls.py file to keep pep8 standard.

--- app/vendor/urls.py
+++ app/vendor/urls.py
@@ -18,19 +18,54 @@ from vendor.views import (
 
 urlpatterns = [
     path('create/', CreateVendorView.as_view(), name='create_vendor'),
-    path('list-create-product', ListCreateProductView.as_view(), name='list-create-product'),
-    path('list-create-product-item', ListCreateProductItemView.as_view(), name='list-create-product-item'),
+    path(
+        'list-create-product',
+        ListCreateProductView.as_view(),
+        name='list-create-product'
+    ),
+
+    path(
+        'list-create-product-item',
+        ListCreateProductItemView.as_view(),
+        name='list-create-product-item'
+    ),
     path(
         'update-delete-product-item/<int:id>',
         RetrieveUpdateDeleteProductItemView.as_view(),
         name='update-delete-product-item'
     ),
     path('update-product', UpdateProductView.as_view(), name='update-product'),
-    path('list-create-category', ListCreateCategoryView.as_view(), name='list-create-category'),
-    path('update-category', UpdateCategoryView.as_view(), name='update-category'),
-    path('list-create-size', ListCreateSizeView.as_view(), name='list-create-size'),
+    path(
+        'list-create-category',
+        ListCreateCategoryView.as_view(),
+        name='list-create-category'
+    ),
+
+    path(
+        'update-category',
+        UpdateCategoryView.as_view(),
+        name='update-category'
+    ),
+
+    path(
+        'list-create-size',
+        ListCreateSizeView.as_view(),
+        name='list-create-size'
+    ),
+
     path('update-size', UpdateSizeView.as_view(), name='update-size'),
-    path('list-create-color', ListCreateColorView.as_view(), name='list-create-color'),
+
+    path(
+        'list-create-color',
+        ListCreateColorView.as_view(),
+        name='list-create-color'
+    ),
+
     path('update-color', UpdateColorView.as_view(), name='update-color'),
-    path('list-create-image', ListCreateImageView.as_view(), name='list-images')
+
+    path(
+        'list-create-image',
+        ListCreateImageView.as_view(),
+        name='list-images'
+    )
 ]
