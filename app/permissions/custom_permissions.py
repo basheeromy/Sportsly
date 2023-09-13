dfa5270076dd32bb4b582a6289a7a7b657cf7244
Muhammed Basheer <basheerkomassery@gmail.com>
2023-09-13 12:12:30 +0530

    Edited custom permission file to handle anonymousUser.

--- app/permissions/custom_permissions.py
+++ app/permissions/custom_permissions.py
@@ -1,7 +1,7 @@
 """Define Custom permissions."""
 
 from rest_framework.permissions import BasePermission
-
+from django.contrib.auth.models import AnonymousUser
 
 class IsSellerUser(BasePermission):
     """
@@ -9,6 +9,8 @@ class IsSellerUser(BasePermission):
     """
 
     def has_permission(self, request, view):
+        if request.user.id == None:
+            return False
         return bool(request.user and request.user.is_seller)
 
 
