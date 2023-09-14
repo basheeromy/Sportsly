76795d7d9453e3a1723c84f46b90a992303f1396
Muhammed Basheer <basheerkomassery@gmail.com>
2023-09-14 08:58:28 +0530

    Created new custom permission to handle oject level permission for vendor related db objects.

--- app/permissions/custom_permissions.py
+++ app/permissions/custom_permissions.py
@@ -21,3 +21,12 @@ class IsOwnerOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
+
+
+class IsSellerOrReadOnly(BasePermission):
+    """
+    Allows access only to owner of the database object
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner
